02fa7fbbe5fa8a719b138ed1c449a80ba4a74271
xGladius <xgladius@protonmail.com>
2020-07-06 00:23:04 -0600

    Update disassembler.cpp
    fixed formatting issues

--- disassembler.cpp
+++ disassembler.cpp
@@ -67,7 +67,8 @@ std::string luau_opnames[0xFF + 1]
 };
 
 template <typename Out>
-void split(const std::string &s, char delim, Out result) {
+void split(const std::string &s, char delim, Out result) 
+{
     std::istringstream iss(s);
     std::string item;
     while (std::getline(iss, item, delim)) {
@@ -75,7 +76,8 @@ void split(const std::string &s, char delim, Out result) {
     }
 }
 
-std::vector<std::string> split(const std::string &s, char delim) {
+std::vector<std::string> split(const std::string &s, char delim) 
+{
     std::vector<std::string> elems;
     split(s, delim, std::back_inserter(elems));
     return elems;
